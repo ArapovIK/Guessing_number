11554bfd46dfbb194fde85b933369df381f1f707
ArapovIK <stricker95@mail.ru>
2022-08-07 23:29:49 +0300

    Добавлена возможность выбора границ, проверка границ, ограниченное количество попыток Осталась проблема с try_limit

--- guessing_number.py
+++ guessing_number.py
@@ -1,23 +1,48 @@
 import random
-from math import ceil, log2, floor
+from math import ceil, log2
 
-print('Привет! Давай сыграем в игру "Угадай число".\nТебе необходимо угадать целое число от 1 до 100.')
+print('Привет! Давай сыграем в игру "Угадай число".\n\nТебе необходимо угадать целое число в заданном диапазоне\
+ за ограниченное количество попыток.\n')
 
-
-def is_valid(num: str) -> bool:
-    '''
+def is_valid(low, high, num: str) -> bool:
+    """
     Проверка корректности вводимых данных
     :param num:
     :return:
-    '''
-    return num.isdigit() and 0 < int(num) < 101 and float(num) % 1 == 0.0
+    """
+    return num.isdigit() and low <= int(num) <= high and float(num) % 1 == 0.0
+
+def is_valid_low_range(edge: str) -> int:
+    """
+    Проверка введенной нижней границы
+    :param edge:
+    :return:
+    """
+    if edge.isdigit():
+        return int(edge)
+    else:
+        edge = input('Введено неверное значение нижней границы. Введите целое число: ')
+        return is_valid_low_range(edge)
+
+def is_valid_high_range(edge: str, low) -> int:
+    """
+    Проверка введенной верхней границы
+    :param edge:
+    :param low:
+    :return:
+    """
+    if edge.isdigit() and int(edge) > low:
+        return int(edge)
+    else:
+        edge = input('Введено неверное значение верхней границы. Введите целое число, первышающее нижнюю границу: ')
+        return is_valid_high_range(edge, low)
 
 def new_game(wish: str) -> bool:
-    '''
+    """
     Запрос на запуск новой игры
     :param wish:
     :return:
-    '''
+    """
     if wish in ['y', 'n']:
         return wish == 'y'
     else:
@@ -26,29 +51,46 @@ def new_game(wish: str) -> bool:
 
 
 def game():
-    number = random.randint(1, 100)
-    attempt = input('Итак, игра началась! Введи предполагаемое число: ')
-    count_try = 1
     while True:
-        if not is_valid(attempt):
-            attempt = input('Неверные данные. Введи целое число от 1 до 100: ')
-            count_try += 1
-            continue
-        elif int(attempt) > number:
-            attempt = input('Введенное число немного МЕНЬШЕ твоего. Попробуй снова: ')
-            count_try += 1
-            continue
-        elif int(attempt) < number:
-            attempt = input('Введенное число немного БОЛЬШЕ твоего. Попробуй снова: ')
-            count_try += 1
+        low_range = is_valid_low_range(input('Введите нижнюю границу диапазона: '))
+        high_range = is_valid_high_range(input('Введите верхнюю границу диапазона: '), low_range)
+        number = random.randint(low_range, high_range)      #выбор случайно числа из заданного диапазона
+        try_limit = ceil(log2(high_range - low_range + 2))      #ограничение количества попыток
+        # + 2 добавлено для определения минимального количества попыток, равного двум
+        count_try = 1       #переменная подсчета количества попыток
+        attempt = input(f'\nИтак, игра началась!\n\
+Загадано число от {low_range} до {high_range}. Количество попыток: {try_limit}. Справишься? {chr(128520)}\n\
+Введи предполагаемое число: ')
+
+        while try_limit >= 1:
+            if not is_valid(low_range, high_range, attempt):
+                try_limit -= 1
+                count_try += 1
+                attempt = input(f'Неверные данные. Осталось попыток: {try_limit}\n\
+Введи целое число от {low_range} до {high_range}: ')
+                continue
+            elif int(attempt) > number:
+                try_limit -= 1
+                count_try += 1
+                attempt = input(f'Загаданное число МЕНЬШЕ вашего. Осталось попыток: {try_limit}\n\
+Попробуй снова: ')
+                continue
+            elif int(attempt) < number:
+                try_limit -= 1
+                count_try += 1
+                attempt = input(f'Загаданное число БОЛЬШЕ вашего. Осталось попыток: {try_limit}\n\
+Попробуй снова: ')
+                continue
+            else:
+                print(f'\nПоздравляю! Ты угадал число {attempt} с {count_try} попытки\nОтличная игра!')
+                break
+        else:
+            print('Упс, попытки закончились...')
+        desire = input('\nХотите сыграть ещё раз? (y - да, n - нет): ')
+        if new_game(desire):    #проверка результата запроса на новую игру
             continue
         else:
-            print(f'\nПоздравляю! Ты угадал число {attempt} с {count_try} попытки')
             break
-    desire = input('\nОтличная игра! Хотите сыграть ещё раз? (y - да, n - нет): ')
-    if new_game(desire):
-        game()
-    else:
-        escape = input('\nСпасибо за игру! Ещё увидимся!\nНажмите Enter для выхода')
+    escape = input('\nСпасибо за игру! Ещё увидимся!\nНажмите Enter для выхода')
 
 game()
