1f7c192743eb60c99cc940973f2966e7192675e5
ArapovIK <stricker95@mail.ru>
2022-09-09 16:04:49 +0300

    Переписал password_gen

--- password_gen.py
+++ password_gen.py
@@ -1,19 +1,31 @@
 import random
+import string
 
-print('Генератор безопасных паролей')
+print('Генератор безопасных паролей\n')
 
-digits = '12345567890'
-lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
-uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
-punctuation = '!#$%&*+-=?@^_'
-chars = ''
+random_base = string.ascii_letters
 
-count_pas = input('Укажите количество паролей: ')
-len_pas = input('Укажите длину одного пароля: ')
+count_pas = int(input('Укажите количество паролей: '))
+len_pas = int(input('Укажите длину одного пароля: '))
 add_digits = input('Включать цифры? (да/нет): ')
-add_uppercase = input('Включать прописные буквы? (да/нет): ')
-add_lowcase = input('Включать строчные буквы? (да/нет): ')
-add_punct = input('Включать спецсимволы "!#$%&*+-=?@^_" ? (да/нет): ')
-exclude_uncertain = input('Исключать неопределенные символы "il1Lo0O" ? (да/нет): ')
+add_punct = input('Включать спецсимволы "%$#@!" ? (да/нет): ')
+exclude_uncertain = input('Исключать неопределенные символы "Ilo0O" ? (да/нет): ')
+
+if add_digits == 'да':
+    random_base += string.digits
+if add_punct == 'да':
+    random_base += "%$#@!"
+if exclude_uncertain == 'да':
+    for symbol in "Ilo0O":
+        if symbol in random_base:
+            random_base = random_base.replace(symbol, '')
 
 
+print('Сгенерированные пароли:')
+
+for _ in range(count_pas):
+    for symbol in random.choices(random_base, k=len_pas):
+        print(symbol, end='')
+    print()
+
+escape = input('Нажмите ENTER для выхода')
